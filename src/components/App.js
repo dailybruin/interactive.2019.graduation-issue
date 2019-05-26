83d70dbbe952f80cd5116271ca5ea5fc2092ec1a
Dustin Newman <dustinnewman98@gmail.com>
2019-05-25 17:58:49 -0700

    added flex layout and emotion

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,8 @@
 import React from "react";
+import { Layout } from "./Layout";
 
 export class App extends React.Component {
     render() {
-        return <h1>Graduation Issue 2019</h1>
+        return <Layout />
     }
 }
