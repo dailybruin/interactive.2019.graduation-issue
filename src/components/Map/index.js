041f040229e7b5056c4f1189f17d72633b601e23
Dustin Newman <dustinnewman98@gmail.com>
2019-05-30 12:31:26 -0700

    map icon draggable

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -1,29 +1,56 @@
 import React from "react";
 import { Map, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 import "leaflet/dist/leaflet.css";
+
+export const mapIcon = new L.Icon({
+    iconUrl: require("../../assets/pawprint.png"),
+    iconRetinaUrl: require("../../assets/pawprint.png"),
+    iconAnchor: [20, 40],
+    popupAnchor: [0, -35],
+    iconSize: [40, 40],
+    shadowSize: [29, 40],
+    shadowAnchor: [7, 40],
+})
 
 export class MyMap extends React.Component {
     constructor(props) {
         super(props);
+        this.refmarker = React.createRef();
         this.state = {
             lat: 51.505,
             lng: -0.09,
             zoom: 13,
+            marker: {
+                lat: 51.505,
+                lng: -0.09,
+            },
         };
+        this.updatePosition = this.updatePosition.bind(this);
+    }
+
+    updatePosition() {
+        const marker = this.refmarker.current;
+        if (marker != null) {
+            this.setState({
+                marker: marker.leafletElement.getLatLng(),
+            })
+        }
     }
 
     render() {
         const position = [this.state.lat, this.state.lng];
+        const markerPosition = [this.state.marker.lat, this.state.marker.lng]
         return (
             <Map center={position} zoom={this.state.zoom}>
                 <TileLayer
                     attribution='&amp;copy <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={position}>
+                <Marker draggable={true} ref={this.refmarker} position={markerPosition} icon={mapIcon} onDragend={this.updatePosition}>
                     <Popup>
                         A pretty CSS3 popup. <br /> Easily customizable.
-            </Popup>
+                    </Popup>
                 </Marker>
             </Map>
         );
