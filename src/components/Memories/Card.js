eddb992f67ab26c1eecf602f8960b9894aa35157
Dustin Newman <dustinnewman98@gmail.com>
2019-06-07 15:47:13 -0700

    stylings

--- src/components/Memories/Card.js
+++ src/components/Memories/Card.js
@@ -1,20 +1,43 @@
 import React from "react";
 import { css } from "emotion";
+import { colors } from "../Shared/colors";
 
 export function Card({ text }) {
     return (
-        <div>
-            <div>
+        <div
+            className={css`
+                background-color: white;
+            `}
+        >
+            <div
+                className={css`
+                    background-color: ${colors.slate};
+                `}
+            >
                 <h3
                     className={css`
                         text-transform: uppercase;
+                        padding: 0.3em 0.5em;
+                        margin: 0;
+                        color: white;
                     `}
                 >
                     I remember...
                 </h3>
             </div>
-            <div>
-                <p>{text}</p>
+            <div
+                className={css`
+                    display: flex;
+                `}
+            >
+                <p
+                    className={css`
+                        padding-left: 0.5em;
+                        line-height: 1.9;
+                    `}
+                >
+                    {text}
+                </p>
             </div>
         </div>
     );
