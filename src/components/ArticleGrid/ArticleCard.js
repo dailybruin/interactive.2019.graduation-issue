a21fa6ffce1d3790b4099158bc8e43ad9da80bbc
Dustin Newman <dustinnewman98@gmail.com>
2019-06-08 13:50:21 -0700

    image magic

--- src/components/ArticleGrid/ArticleCard.js
+++ src/components/ArticleGrid/ArticleCard.js
@@ -9,6 +9,7 @@ export default function ArticleCard({ article }) {
                 border: 6px solid ${colors.blue};
                 background-color: ${colors.fills.blue};
                 padding: 0.7em;
+                align-self: start;
             `}
         >
             <a
@@ -20,11 +21,12 @@ export default function ArticleCard({ article }) {
                 `}
             >
                 <img
-                    className={css`
-                        max-width: 100%;
-                    `}
                     src={article.image}
-                    alt={article.imagedescription}
+                    className={css`
+                        object-fit: cover;
+                        width: 100%;
+                        max-height: 250px;
+                    `}
                 />
                 <h3 className="headline">
                     {article.italics ? (
