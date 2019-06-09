fb0fd5d688d702a4ff63caa755066c3f112afa30
tom22ger <thomasadrian@mac.com>
2019-06-08 22:00:10 -0700

    Added Cookie for Landing Page

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 import reducer from "./reducers";
 import { App } from "./components/App";
 
@@ -9,10 +9,39 @@ const store = createStore(reducer);
 
 // SSR (Server Side Rendering) safe
 if (document != null) {
-    ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById("root"));
+    ReactDOM.render(
+        <Provider store={store}>
+            <App />
+        </Provider>,
+        document.getElementById("root")
+    );
 
     // HMR (Hot Module Replacement)
     if (module.hot) {
         module.hot.accept();
     }
+
+    // Cookies for Landing Page
+    if (checkVistiedCookie()) {
+        document.getElementById("LandingPage").style.display = "none";
+    }
+    setVisitedCookie();
+}
+
+// Cookie Utilities
+function setVisitedCookie() {
+    var date = new Date();
+    date.setTime(date.getTime() + 2 * 24 * 60 * 60 * 1000);
+    var expires = "expires=" + date.toUTCString();
+    document.cookie = "visited=true;" + expires + ";path=/";
+}
+
+function checkVistiedCookie() {
+    var cookies = document.cookie.split(";");
+    for (var i = 0; i < cookies.length; i++) {
+        if (cookies[i] == "visited=true") {
+            return true;
+        }
+    }
+    return false;
 }
